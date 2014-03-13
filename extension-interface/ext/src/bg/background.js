b3d35570b9f10217f35d250c8605abaa58bbbd2e
caidavies <hello@caidavies.me>
2014-03-13 01:25:02 +0000

    Applied separate feeds
    Applied separate feeds for leaderboard. Sending score from background
    to popup DOM needed.

--- extension-interface/ext/src/bg/background.js
+++ extension-interface/ext/src/bg/background.js
@@ -1,4 +1,37 @@
 
+// MQTT for leaderboard
+
+var client = new Messaging.Client("box.bento.is", 8080, "myclientid_" + parseInt(Math.random() * 100, 10));
+
+var options = {
+
+  //connection attempt timeout in seconds
+  timeout: 3,
+
+  //Gets Called if the connection has successfully been established
+  onSuccess: function () {
+    console.log("Connected");
+    client.subscribe("cai-score");
+    client.subscribe("chris-score");
+    client.subscribe("aaron-score");
+  },
+
+
+  //Gets Called if the connection could not be established
+  onFailure: function (message) {
+    console.log("Connection failed: " + message.errorMessage);
+    client.connect(options);
+  }
+
+};
+client.connect(options);
+
+
+  client.onMessageArrived = function (message) {
+    updateLeaderboard(message.destinationName, message.payloadString);
+  }
+
+
 // Set Badge Icon
 chrome.browserAction.setBadgeBackgroundColor({color: "#000"});
 
@@ -21,3 +54,29 @@ chrome.extension.onMessage.addListener(
   	chrome.pageAction.show(sender.tab.id);
     sendResponse();
   });
+
+//   Functions
+function updateLeaderboard(topic, score)
+{
+   switch (topic)
+   {
+
+     case "cai-score":
+     $('#cai .score').text(score);
+     console.log(score);
+     break;
+
+     case "chris-score":
+     $('#chris .score').text(score);
+     console.log('score logged');
+     break;
+
+     case "aaron-score":
+     $('#aaron .score').text(score);
+     console.log('score logged');
+     break;
+
+     default:
+       return false;
+   }
+}
