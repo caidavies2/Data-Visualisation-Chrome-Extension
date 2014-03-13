f2d67d7ad3914563b58c9eddc2c249b613721725
caidavies <hello@caidavies.me>
2014-03-13 23:41:10 +0000

    Tweaked substrings

--- extension-interface/ext/src/browser_action/popup.js
+++ extension-interface/ext/src/browser_action/popup.js
@@ -1,17 +1,33 @@
 // console.log(background);
 // background.transfer;
 
-var background;
-
-setInterval(function(){
-console.log(background);
-}, 2000);
-
 $(document).ready(function(){
+
     backgroundCai = chrome.extension.getBackgroundPage().caiScoreTally;
     backgroundChris = chrome.extension.getBackgroundPage().chrisScoreTally;
     backgroundAaron = chrome.extension.getBackgroundPage().aaronScoreTally;
-    console.log(background);
+
+    if(typeof backgroundCai == "undefined")
+    {
+      backgroundCai = "0";
+      $('#cai .score').text("--");
+    }
+    else if (typeof backgroundChris == "undefined")
+    {
+      backgroundChris = "0";
+      $('#chris .score').text("--");
+    }
+    else if (typeof backgroundAaron == "undefined")
+    {
+      backgroundAaron = "0";
+      $('#aaron .score').text("--");
+    }
+
+
+    backgroundCai = backgroundCai.substr(0,7);
+    backgroundChris = backgroundChris.substr(0,7);
+    backgroundAaron = backgroundAaron.substr(0,7);
+
     $('#cai .score').text(backgroundCai);
     $('#chris .score').text(backgroundChris);
     $('#aaron .score').text(backgroundAaron);
