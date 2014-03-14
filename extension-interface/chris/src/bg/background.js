89f1a9199f7a78950a0dba3a6f3ba31f8d0892e7
caidavies <hello@caidavies.me>
2014-03-14 01:56:05 +0000

    Added individual Extensions

---
+++ extension-interface/chris/src/bg/background.js
@@ -0,0 +1,73 @@
+
+// MQTT for leaderboard
+var caiScoreTally,aaronScoreTally,chrisScoreTally
+
+var client = new Messaging.Client("box.bento.is", 8080, "myclientid_" + parseInt(Math.random() * 100, 10));
+
+var options = {
+
+  //connection attempt timeout in seconds
+  timeout: 3,
+
+  //Gets Called if the connection has successfully been established
+  onSuccess: function () {
+    console.log("Connected");
+    client.subscribe("cai_final_score");
+    client.subscribe("evans_final_score");
+    client.subscribe("aaron_final_score");
+  },
+
+
+  //Gets Called if the connection could not be established
+  onFailure: function (message) {
+    console.log("Connection failed: " + message.errorMessage);
+    client.connect(options);
+  }
+
+};
+client.connect(options);
+
+
+client.onMessageArrived = function (message) {
+  updateLeaderboard(message.destinationName, message.payloadString);
+}
+
+
+// Set Badge Icon
+chrome.browserAction.setBadgeBackgroundColor({color: "#000"});
+
+var i = 1;
+
+setInterval(function(){
+  i = i.toString();
+  updateBadge(i);
+  i++
+}, 1000);
+
+function updateBadge(nOnline)
+{
+  // chrome.browserAction.setBadgeText({text: nOnline});
+}
+
+  //   Functions
+  function updateLeaderboard(topic, score)
+  {
+    switch (topic)
+    {
+    case "cai_final_score":
+
+      caiScoreTally = score;
+      break;
+
+    case "evans_final_score":
+      chrisScoreTally = score;
+      break;
+
+    case "aaron_final_score":
+      aaronScoreTally = score;
+      break;
+
+    default:
+      return false;
+    }
+  }
